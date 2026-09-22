28c57ff7c4e5ced73eeb2ede8a8c95cd3cd8aa3f
msdfsf <fsfdsm@gmail.com>
2026-09-22 02:32:19 +0200

    Updated config.h

--- src/config.h
+++ src/config.h
@@ -4,25 +4,26 @@ namespace Config {
         // Set to true for LSP/Library builds
         bool errorRecoveryEnabled = false;
 
-    // Set to true to enable logging
-    #if defined(CONFIG_DISABLE_LOGGING)
-        constexpr bool LOGGING_ENABLED = false;
-    #else
-        constexpr bool LOGGING_ENABLED = true;
-    #endif
+        // Set to true to enable logging
+        bool loggingEnabled = true;
+
+        int maxErrorCount = 100;
+
+        int threadCount         = 12;
+        int threadWorkQueueSize = 32;
+
+        // Starting file count to pre-allocate space for
+        int initFileCount = 128;
+
+        // expected namespace count per file used
+        // ex. to pre-allocate sets
+        int expectedNamespaceCount = 32;
+    };
 
     constexpr int LINEAR_SEARCH_THRESHOLD = 16;
 
-    extern int maxErrorCount;
-
-    extern int threadCount;
-    extern int threadWorkQueueSize;
-
-    // starting file count to pre-allocate space for
-    extern int fileCount;
-
-    // expected namespace count per file used
-    // ex. to pre-allocate sets
-    extern int expectedNamespaceCount;
+    // TODO: For now global, but there may be need to apply different options
+    //       for different compilations within the very same program...
+    extern Options opt;
 
 }
